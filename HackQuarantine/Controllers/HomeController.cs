1241dd745646d8f6ff2c8079906a84ad2469d510
Nevada Graci <gracine@sheridancollege.ca>
2020-04-05 16:37:56 -0400

    Request form functionality added

--- HackQuarantine/Controllers/HomeController.cs
+++ HackQuarantine/Controllers/HomeController.cs
@@ -12,6 +12,8 @@ namespace HackQuarantine.Controllers
 {
     public class HomeController : Controller
     {
+        static int[] idArray;
+
         public IActionResult Index()
         {
             return View();
@@ -20,40 +22,38 @@ namespace HackQuarantine.Controllers
         [HttpGet]
         public IActionResult Privacy()
         {
+            return View(Repository._stores);
+        }
+
+        [HttpPost]
+        public IActionResult Privacy(string storeId, string itemId)
+        {
+            var x = int.Parse(storeId);
+            var y = int.Parse(itemId);
+            idArray = new int[] { x, y };
+            return View("CommentForm");
+        }
+
+        [HttpGet]
+        public IActionResult CommentForm()
+        {
             return View();
         }
 
-        //public IActionResult CommentRequest()
-        //{
-        //    var model = new CommentRequest { };
-        //    return PartialView("CommentModalPartial", model);
-        //}
-
-        //[HttpPost]
-        //public IActionResult CommentRequest(CommentRequest model)
-        //{
-        //    return PartialView("CommentModalPartial", model);
-        //}
-
-
         [HttpPost]
-        public ViewResult Privacy(CommentRequest request)
+        public ViewResult CommentForm(CommentRequest commentRequest)
         {
+            commentRequest.StoreId = idArray[0];
+            commentRequest.ItemId = idArray[1];
             if (ModelState.IsValid)
             {
-                Repository.AddComment(request);
-                return View();
+                Repository.AddComment(commentRequest);
+                return View("Privacy");
             }
             else
             {
-                return View("Index");
+                return View();
             }
         }
-
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
     }
 }
