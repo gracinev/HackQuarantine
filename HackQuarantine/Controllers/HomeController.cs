d07192e9cd87fd164175f29267037b566ddbb460
Nan Li <nanli@Nans-MacBook-Air.local>
2020-04-07 17:11:36 -0700

    working things in StoreController

--- HackQuarantine/Controllers/HomeController.cs
+++ HackQuarantine/Controllers/HomeController.cs
@@ -10,14 +10,6 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System.Web;
 using System.Net;
 
-// Classes we'll use from the GoogleApi package maybe?
-using GoogleApi;
-using GoogleApi.Entities.Common;
-using GoogleApi.Entities.Common.Enums;
-using GoogleApi.Entities.Places.Details.Request;
-using GoogleApi.Entities.Places.Photos.Request;
-using GoogleApi.Entities.Places.Search.NearBy.Request;
-// End of Google API classes
 
 namespace HackQuarantine.Controllers
 {
@@ -112,30 +104,7 @@ namespace HackQuarantine.Controllers
         }
 
 
-        // Trying some Google Maps stuff below
-        // From https://github.com/vivet/GoogleApi
-        // From https://www.jerriepelser.com/tutorials/airport-explorer/google-places/retrieving/
-        // but it's for Razor pages?
-        // Looked at https://www.youtube.com/watch?v=3NLfhEjq9Tk
-        // GET Home/StoreDetail?
-        public async Task<IActionResult> StoreDetail()
-        {
-            // For testing the call
-            double latitude = 37.424831;
-            double longitude = -121.867796;
-            var searchResponse = await GooglePlaces.NearBySearch.QueryAsync(new PlacesNearBySearchRequest
-            {
-                Key = "AIzaSyBqAlYsHSmQ7giaR-rSd9tLRZhztugU004",
-                Location = new GoogleApi.Entities.Places.Search.NearBy.Request.Location(latitude, longitude),
-                Radius = 10000
-            });
 
-            if (!searchResponse.Status.HasValue || searchResponse.Status.Value != Status.Ok || !searchResponse.Results.Any())
-                return new BadRequestResult();
-
-            // How do I pass response to View()
-            // I can't find documentation on this nuget package 
-            return View(searchResponse.Results);
         }
     }
 }
--- HackQuarantine/Controllers/StoreController.cs
+++ HackQuarantine/Controllers/StoreController.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+// Classes we'll use from the GoogleApi package maybe?
+using GoogleApi;
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Common.Enums;
+using GoogleApi.Entities.Places.Details.Request;
+using GoogleApi.Entities.Places.Photos.Request;
+using GoogleApi.Entities.Places.Search.NearBy.Request;
+// End of Google API classes
+
+
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HackQuarantine.Controllers
@@ -16,4 +26,29 @@ namespace HackQuarantine.Controllers
             return View();
         }
     }
-}
+
+    // Trying some Google Maps stuff below
+    // From https://github.com/vivet/GoogleApi
+    // From https://www.jerriepelser.com/tutorials/airport-explorer/google-places/retrieving/
+    // but it's for Razor pages?
+    // Looked at https://www.youtube.com/watch?v=3NLfhEjq9Tk
+    // GET Home/StoreDetail?
+    public async Task<IActionResult> StoreDetail()
+    {
+        // For testing the call
+        double latitude = 37.424831;
+        double longitude = -121.867796;
+        var searchResponse = await GooglePlaces.NearBySearch.QueryAsync(new PlacesNearBySearchRequest
+        {
+            Key = "AIzaSyBqAlYsHSmQ7giaR-rSd9tLRZhztugU004",
+            Location = new GoogleApi.Entities.Places.Search.NearBy.Request.Location(latitude, longitude),
+            Radius = 10000
+        });
+
+        if (!searchResponse.Status.HasValue || searchResponse.Status.Value != Status.Ok || !searchResponse.Results.Any())
+            return new BadRequestResult();
+
+        // How do I pass response to View()
+        // I can't find documentation on this nuget package 
+        return View(searchResponse.Results);
+    }
