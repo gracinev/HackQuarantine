79921d657c63e9d41f47d3531b801176e711db02
Ben Zeero <bentyh@hotmail.com>
2020-04-10 15:14:31 -0700

    View and Placeholder Creation
    Include user landing and setting page.
    Only display, non-function.

--- HackQuarantine/Controllers/HomeController.cs
+++ HackQuarantine/Controllers/HomeController.cs
@@ -35,6 +35,16 @@ namespace HackQuarantine.Controllers
             return View();
         }
 
+        public IActionResult UserEntrance()
+        {
+            return View();
+        }
+
+        public IActionResult UserSetting()
+        {
+            return View();
+        }
+
         [HttpPost]
         public ActionResult AddStores([FromBody]List<GoogleStore> stores)
         {
